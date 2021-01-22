a15c4872bdc7362dc3d5bfef78dd1cad92837581
Kent Roth <kentencroth@yahoo.com>
2021-01-22 15:52:10 -0600

    setup

---
+++ Jan/jan.js
@@ -0,0 +1,14 @@
+// check to see if a string has the same amount of x's and o's
+// must return boolean
+// must be case insensitive
+// if no x and o should return true
+
+function XO(str) {
+	console.log(str);
+}
+
+XO('xo'); // true
+XO('xooxx'); // false
+XO('ooxXm'); // true
+XO('zpzpzpp'); // true
+XO('zzoo'); // false
