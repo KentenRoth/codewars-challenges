d2f0bd1912b17f37b1376a2c5c2ba1328bcbd479
Kent Roth <kentencroth@yahoo.com>
2021-01-23 14:37:28 -0600

    Money Money Money

--- Jan/jan23.js
+++ Jan/jan23.js
@@ -3,7 +3,21 @@
 // Need starting amount, interest, tax, and desired end amount
 
 function calculateYears(principal, interest, tax, desired) {
-	// Figure it out
+	let years = 0;
+	let yearlyPrincipal = principal;
+	if (principal >= desired) {
+		console.log(years);
+		return years;
+	}
+	do {
+		let currentPrincipal = yearlyPrincipal;
+		let taxedAmount = currentPrincipal * interest * tax;
+		let profit = currentPrincipal * interest - taxedAmount;
+		let newPrincipal = profit + currentPrincipal;
+		yearlyPrincipal = newPrincipal;
+		years++;
+	} while (yearlyPrincipal < desired);
+	return console.log(years);
 }
 
 calculateYears(1000, 0.05, 0.18, 1100); // 3 years
