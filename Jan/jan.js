2c33e88df4784897a16651649b45bb319fe528b9
Kent Roth <kentencroth@yahoo.com>
2021-01-22 18:42:23 -0600

    exes and ohs completed

--- Jan/jan.js
+++ Jan/jan.js
@@ -1,10 +1,28 @@
+// Exes and Ohs
 // check to see if a string has the same amount of x's and o's
 // must return boolean
 // must be case insensitive
 // if no x and o should return true
 
 function XO(str) {
-	console.log(str);
+	let x = 0;
+	let o = 0;
+	let string = str.toLowerCase();
+	for (let i = 0; i < string.length; i++) {
+		if (string[i] === 'x') {
+			x = x + 1;
+		} else if (string[i] === 'o') {
+			o = o + 1;
+		}
+	}
+
+	if (x === o) {
+		console.log(true);
+		return true;
+	} else {
+		console.log(false);
+		return false;
+	}
 }
 
 XO('xo'); // true
