f4dbd091cee6670cb94817bad311e1dcb0a6a3b4
Kent Roth <kentencroth@yahoo.com>
2021-01-23 13:38:16 -0600

    Setup

---
+++ Jan/jan23.js
@@ -0,0 +1,11 @@
+// Money Money Money
+// Invest money until reaching the desired amount
+// Need starting amount, interest, tax, and desired end amount
+
+function calculateYears(principal, interest, tax, desired) {
+	// Figure it out
+}
+
+calculateYears(1000, 0.05, 0.18, 1100); // 3 years
+calculateYears(1000, 0.01625, 0.18, 1200); // 14 years
+calculateYears(1000, 0.05, 0.18, 1000); // 0 years
