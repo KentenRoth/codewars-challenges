1611475a2c2332531909ed9763f3558327fda907
Kent Roth <kentencroth@yahoo.com>
2021-01-24 12:51:55 -0600

    RGB To HEX
    This one I think is going to be difficult.

---
+++ Jan/jan24.js
@@ -0,0 +1,15 @@
+// RGB to HEX conversion
+// Take number and divide by 16
+// take remainder of number multiply by 16
+
+// 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 Decimal
+// 0 1 2 3 4 5 6 7 8 9 A B C D  E  F  Hexadecimal
+
+function rgbToHex(r, g, b) {
+	// Code
+}
+
+rgbToHex(0, 0, 0); //'000000'
+rgbToHex(0, 0, -20); //'000000'
+rgbToHex(300, 255, 255); //'FFFFFF'
+rgbToHex(173, 255, 47); //'ADFF2F'
