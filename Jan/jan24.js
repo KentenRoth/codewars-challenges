af7550e705385089f3bde9122634dc1d1f1651f0
Kent Roth <kentencroth@yahoo.com>
2021-01-24 12:59:25 -0600

    Setup
    This will need major refactoring

--- Jan/jan24.js
+++ Jan/jan24.js
@@ -5,8 +5,50 @@
 // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 Decimal
 // 0 1 2 3 4 5 6 7 8 9 A B C D  E  F  Hexadecimal
 
+// Index postion will give the hexadecimal
+let conversion = [
+	'0',
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8',
+	'9',
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+];
+
 function rgbToHex(r, g, b) {
-	// Code
+	// Each argument will have its own hex code
+	// need to combine each hex code when finished
+	let rHex = '';
+	let gHex = '';
+	let bHex = '';
+
+	if (r > 0) {
+		// code
+	} else {
+		rHex === 00;
+	}
+	if (g > 0) {
+	} else {
+		gHex === 00;
+	}
+
+	if (b > 0) {
+		// code
+	} else {
+		bHex === 000;
+	}
+
+	console.log(rhex, gHex, bHex);
 }
 
 rgbToHex(0, 0, 0); //'000000'
