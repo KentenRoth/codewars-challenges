5fe3ee18658cfdf45fa3cc0105ece2dd88fb5218
Kent Roth <kentencroth@yahoo.com>
2021-01-26 11:26:14 -0600

    Setup

---
+++ jan26.js
@@ -0,0 +1,13 @@
+// +1 Array
+
+// Array cannot be empty
+// only positive numbers, only single digit numbers allowed
+// return null for invalid inputs
+
+function upArray(arr) {
+	// Code
+}
+
+upArray([2, 3, 9]); // [2,4,0]
+upArray([4, 3, 2, 5]); // [4,3,2,6]
+upArray([1, -9]); // null
