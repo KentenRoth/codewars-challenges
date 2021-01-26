4698a7a9039a055044810f745ac91f6ccca70ad8
Kent Roth <kentencroth@yahoo.com>
2021-01-26 14:24:42 -0600

    missing edge cases

--- Jan/jan26.js
+++ Jan/jan26.js
@@ -6,16 +6,61 @@
 
 function upArray(arr) {
 	let killSwitch = false;
-	arr.map((num) => {
-		if (num < 0 || isNaN(num) === false) {
-			return (killSwitch = true);
-		}
-	});
-	if (killSwitch === true) {
-		return null;
+	let intArray = Number(arr.join(''));
+	let addedOne = intArray + 1;
+	if (arr.some(isNaN)) {
+		console.log('hello');
 	}
+	if (isNaN(intArray) === true) {
+		killSwitch = true;
+	}
+	if (killSwitch === true) {
+		return console.log('null');
+	}
+	let numString = addedOne.toString().split('');
+	return console.log(numString.map(Number));
 }
 
-upArray([2, 3, 9]); // [2,4,0]
-upArray([4, 3, 2, 5]); // [4,3,2,6]
-upArray([1, -9]); // null
+// upArray([2, 3, 9]); // [2,4,0]
+// upArray([4, 3, 2, 5]); // [4,3,2,6]
+// upArray([1, -9]); // null
+upArray([9, 2, 2, 3, 3, 7, 2, 0, 3, 6, 8, 5, 4, 7, 7, 5, 8, 0, 8]); // [9, 2, 2, 3, 3, 7, 2, 0, 3, 6, 8, 5, 4, 7, 7, 5, 8, 0, 9]
+upArray([
+	9,
+	2,
+	2,
+	3,
+	3,
+	7,
+	2,
+	0,
+	3,
+	6,
+	8,
+	5,
+	4,
+	7,
+	7,
+	5,
+	8,
+	0,
+	7,
+	5,
+	3,
+	2,
+	6,
+	7,
+	8,
+	4,
+	2,
+	4,
+	2,
+	6,
+	7,
+	8,
+	7,
+	4,
+	5,
+	2,
+	2,
+]);
