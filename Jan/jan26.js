85bcd39c2ad311878297faab957782090413aa04
Kent Roth <kentencroth@yahoo.com>
2021-01-26 15:00:16 -0600

    Close to completing
    Still not working for large numbers.  Need to find something to work better for that.

--- Jan/jan26.js
+++ Jan/jan26.js
@@ -6,24 +6,32 @@
 
 function upArray(arr) {
 	let killSwitch = false;
-	let intArray = Number(arr.join(''));
-	let addedOne = intArray + 1;
-	if (arr.some(isNaN)) {
-		console.log('hello');
+	let stringArray = arr.join('');
+	let intArray = ++stringArray;
+
+	arr.map((num) => {
+		if (num > 9 || num < 0) {
+			killSwitch = true;
+		}
+	});
+
+	if (arr.length === 0) {
+		killSwitch = true;
 	}
+
 	if (isNaN(intArray) === true) {
 		killSwitch = true;
 	}
 	if (killSwitch === true) {
-		return console.log('null');
+		return null;
 	}
-	let numString = addedOne.toString().split('');
-	return console.log(numString.map(Number));
+
+	return intArray;
 }
 
-// upArray([2, 3, 9]); // [2,4,0]
-// upArray([4, 3, 2, 5]); // [4,3,2,6]
-// upArray([1, -9]); // null
+upArray([2, 3, 9]); // [2,4,0]
+upArray([4, 3, 2, 5]); // [4,3,2,6]
+upArray([1, -9, 12]); // null
 upArray([9, 2, 2, 3, 3, 7, 2, 0, 3, 6, 8, 5, 4, 7, 7, 5, 8, 0, 8]); // [9, 2, 2, 3, 3, 7, 2, 0, 3, 6, 8, 5, 4, 7, 7, 5, 8, 0, 9]
 upArray([
 	9,
